c1279627e9a852192ca72e4f5059755f087f5217
Ocejo <alejandro.ocejo@dxc.com>
2023-10-05 12:09:52 +0200

    añadidos Characters y Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
+import { cleanup } from "@testing-library/react";
+
+
 
 function App() {
+
+  const [characters, setCharacters] = useState([]);
+
+  const initialUrl = "https://rickandmortyapi.com/api/character"
+
+  const fetchCharacter = (url) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setCharacters(data.results))
+      .catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    fetchCharacter(initialUrl);
+  }, [])
+
   return (
-    <Navbar brand="Rick and Morty APP" />
+    <>
+      <Navbar brand="Rick and Morty APP" />
+
+      <div className="container">
+
+      </div>
+    </>
   );
 }
 
